f70e26ac9717c8f438f66b8c91f4ca1944750ee7
Lucas Delobelle <lucas@ld-web.net>
2024-05-02 13:04:20 +0200

    :sparkles: add Tailwind

--- curl/index.php
+++ curl/index.php
@@ -14,13 +14,28 @@ $users = getUsers();
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
-<body>
-    <form action=""></form>
-
-    <div class="flex gap-2 flex-wrap">
+<body class="bg-gray-900 text-gray-200">
+    <h1 class="mt-16 text-center text-5xl">Users JSONPlaceholder</h1>
+    <div class="flex gap-4 flex-wrap justify-center mx-auto mt-8 mb-12">
         <?php foreach ($users as $user) { ?>
-        <div class="border-2 p-2">
-            <?php echo $user['name']; ?>
+        <div class="border-[1px] p-2 rounded-md w-[300px]">
+            <h2 class="text-center text-2xl">
+                <?php echo $user['name']; ?>
+            </h2>
+            <div class="mt-4">
+                <p class="flex gap-2">
+                    <img src="icons/email.svg" alt="Email" class="w-[20px]" />
+                    <?php echo $user['email']; ?>
+                </p>
+                <p class="flex gap-2">
+                    <img src="icons/phone.svg" alt="Phone" class="w-[20px]" />
+                    <?php echo $user['phone']; ?>
+                </p>
+                <p class="flex gap-2">
+                    <img src="icons/website.svg" alt="Website" class="w-[20px]" />
+                    <?php echo $user['website']; ?>
+                </p>
+            </div>
         </div>
         <?php } ?>
     </div>
